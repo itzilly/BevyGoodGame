3f4486c7aca215a7f0ac68664f0ab5d85099e68a
illyum <90023277+itzilly@users.noreply.github.com>
2023-01-18 13:46:28 -0700

    We have a window

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
+#![allow(unused)]
+#![allow(non_snake_case)]
+
+
+use bevy::prelude::*;
+
+
 fn main() {
-    println!("Time to make a game!");
-    println!("lets do it")
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            window: WindowDescriptor {
+                title: "Bevy Good Game".to_string(),
+                width: 400.0,
+                height: 700.0,
+                ..Default::default()
+            },
+            ..Default::default()
+        }))
+        .run();
 }
