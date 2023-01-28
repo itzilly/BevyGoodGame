e7a5b14a3bde0ee001fc0d604e1be1aad3ddbad6
qsters <qqgrant@gmail.com>
2023-01-27 17:05:12 -0700

    Refactored and implemented new player movement system

--- src/game_core/player.rs
+++ src/game_core/player.rs
@@ -1,19 +1,27 @@
 use bevy::prelude::*;
+use bevy_inspector_egui::egui::Event::Key;
 use super::attack::Attack;
+use bevy_rapier2d::prelude::*;
 
 #[derive(Component)]
 pub struct Player {
     pub health: i32,
     pub attack_power: i32,
     pub armor: i32,
+    pub acceleration: f32,
+    pub deceleration: f32,
+    pub max_speed: f32,
 }
 
 impl Player {
-    pub fn new(health: i32, attack_power: i32, armor: i32) -> Player {
+    pub fn new(health: i32, attack_power: i32, armor: i32, acceleration: f32, deceleration: f32, max_speed: f32) -> Player {
         Player {
             health,
             attack_power,
             armor,
+            acceleration,
+            deceleration,
+            max_speed,
         }
     }
 }
@@ -24,51 +32,70 @@ impl Attack for Player {
     }
 }
 
-#[derive(Component)]
-pub enum Direction {
-    Up,
-    Down
-}
+pub fn player_movement_system(time: Res<Time>, keyboard_input: Res<Input<KeyCode>>, mut player: Query<(&mut Velocity, &Player, &Transform)>) {
+    // Get player information
+    if let Ok((mut player_velocity, player, transform)) = player.get_single_mut() {
+
+        // If any movement keys pressed
+        if keyboard_input.any_pressed([KeyCode::Up, KeyCode::W, KeyCode::Left, KeyCode::A, KeyCode::Down, KeyCode::S, KeyCode::Right, KeyCode::D]) {
+
+            // Acceleration is the rate at wich the speed increases
+            let mut acceleration = Vec2::ZERO;
+
+            // set the acceleration based on inputs
+            if keyboard_input.pressed(KeyCode::Up) || keyboard_input.pressed(KeyCode::W) {
+                acceleration += Vec2::new(0.0, 1.0) * player.acceleration;
+            }
+            if keyboard_input.pressed(KeyCode::Right) || keyboard_input.pressed(KeyCode::D) {
+                acceleration += Vec2::new(1.0, 0.0) * player.acceleration;
+            }
+            if keyboard_input.pressed(KeyCode::Down) || keyboard_input.pressed(KeyCode::S) {
+                acceleration += Vec2::new(0.0, -1.0) * player.acceleration;
+            }
+            if keyboard_input.pressed(KeyCode::Left) || keyboard_input.pressed(KeyCode::A) {
+                acceleration += Vec2::new(-1.0, 0.0) * player.acceleration;
+            }
+
+            // Only if the acceleration is zero (ONLY IN CASES WHERE ALL ARE PRESSED)
+            if acceleration.length_squared() == 0.0 {
+                return;
+            }
+
+            // making sure the diagonals arent faster than they should be
+            acceleration = acceleration.normalize();
+
+            let velocity_dot_acceleration = player_velocity.linvel.normalize().dot(acceleration.normalize());
+
+            // When changing directions, accelerate more to make the turn faster
+            if velocity_dot_acceleration < 0.9 {
+                acceleration = acceleration * 4.0;
+            }
+            else if velocity_dot_acceleration > 0.9 && velocity_dot_acceleration < 0.98 {
+                acceleration = (acceleration.normalize() - (player_velocity.linvel.normalize() * 0.8)).normalize()
+            }
 
 
 
-pub fn sprite_movement(time: Res<Time>, keyboard_input: Res<Input<KeyCode>>, mut sprite_position: Query<(&mut Direction, &mut Transform), With<Player>>) {
-    for (mut logo, mut transform) in &mut sprite_position {
-        match *logo {
-            Direction::Up => {
-                if keyboard_input.pressed(KeyCode::Up) {
-                    transform.translation.y += 150. * time.delta_seconds();
-                }
-                if keyboard_input.pressed(KeyCode::Down) {
-                    transform.translation.y -= 150. * time.delta_seconds();
-                }
-                if keyboard_input.pressed(KeyCode::Left) {
-                    transform.translation.x -= 150. * time.delta_seconds();
-                }
-                if keyboard_input.pressed(KeyCode::Right) {
-                    transform.translation.x += 150. * time.delta_seconds();
-                }
-            },
-            Direction::Down => {
-                if keyboard_input.pressed(KeyCode::Up) {
-                    transform.translation.y += 150. * time.delta_seconds();
-                }
-                if keyboard_input.pressed(KeyCode::Down) {
-                    transform.translation.y -= 150. * time.delta_seconds();
-                }
-                if keyboard_input.pressed(KeyCode::Left) {
-                    transform.translation.x -= 150. * time.delta_seconds();
-                }
-                if keyboard_input.pressed(KeyCode::Right) {
-                    transform.translation.x += 150. * time.delta_seconds();
-                }
-            },
+
+            // apply the velocity
+            player_velocity.linvel += acceleration * player.acceleration * time.delta_seconds();
+
+            // Cap speed if beyond max
+            if player_velocity.linvel.length() > player.max_speed {
+                player_velocity.linvel = player_velocity.linvel.normalize() * player.max_speed;
+            }
+            // If no input AND if after this loop the velocity will be greater than zero
+        } else if player_velocity.linvel.length() - (player.deceleration * time.delta_seconds()) > 0.0 {
+            // Decelerate
+            player_velocity.linvel = player_velocity.linvel - player_velocity.linvel.normalize() * player.deceleration * time.delta_seconds();
+            return;
+        } else { // Stop the player
+            player_velocity.linvel = Vec2::ZERO;
+            return;
         }
 
-        if transform.translation.y > 200. {
-            *logo = Direction::Down;
-        } else if transform.translation.y < -200. {
-            *logo = Direction::Up;
-        }
+
     }
+
+
 }
--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,11 @@ use bevy::window::{CompositeAlphaMode, CursorGrabMode, PresentMode, WindowResize
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use bevy_ecs_ldtk::prelude::*;
 use bevy_rapier2d::prelude::*;
-use bevy_rapier2d::rapier::prelude;
-use std::collections::{HashMap, HashSet};
 
 use game_core::player;
 use game_core::attack;
+use game_core::world;
 use crate::game_core::player::Player;
-
-#[derive(
-Copy, Clone, Eq, PartialEq, Debug, Default, Component,
-)]
-struct Fence;
-
-#[derive(Clone, Debug, Default, Bundle, LdtkIntCell)]
-struct FenceBundle {
-    fence: Fence,
-}
 
 
 fn main() {
@@ -56,199 +45,19 @@ fn main() {
     }))
         .add_plugin(RapierDebugRenderPlugin::default())
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
+        .add_plugin(LdtkPlugin)
         .insert_resource(LevelSelection::Index(0))
-        .register_ldtk_int_cell::<FenceBundle>(1)
+        .register_ldtk_int_cell::<world::FenceBundle>(1)
 
         // .add_plugin(WorldInspectorPlugin);  // -> Debug information panel
-        .add_plugin(LdtkPlugin)
+
         .add_startup_system(setup_system)
         .insert_resource(LevelSelection::Index(0))
-
-        .add_system(player::sprite_movement)
-        .add_system(spawn_fence_collision);
+        .add_system(player::player_movement_system)
+        .add_system(world::spawn_fence_collision);
 
     app.run();
 }
-
-
-fn spawn_fence_collision(
-    mut commands: Commands,
-    wall_query: Query<(&GridCoords, &Parent), Added<Fence>>,
-    parent_query: Query<&Parent, Without<Fence>>,
-    level_query: Query<(Entity, &Handle<LdtkLevel>)>,
-    levels: Res<Assets<LdtkLevel>>,
-) {
-    /// Represents a wide wall that is 1 tile tall
-    /// Used to spawn wall collisions
-    #[derive(
-    Copy, Clone, Eq, PartialEq, Debug, Default, Hash,
-    )]
-    struct Plate {
-        left: i32,
-        right: i32,
-    }
-
-    /// A simple rectangle type representing a
-    /// wall of any size
-    #[derive(
-    Copy, Clone, Eq, PartialEq, Debug, Default, Hash,
-    )]
-    struct Rect {
-        left: i32,
-        right: i32,
-        top: i32,
-        bottom: i32,
-    }
-
-    // Consider where the walls are
-    // storing them as GridCoords in a HashSet for
-    // quick, easy lookup
-    //
-    // The key of this map will be the entity of the
-    // level the wall belongs to. This has two
-    // consequences in the resulting collision
-    // entities: 1. it forces the walls to be
-    // split along level boundaries 2. it lets us
-    // easily add the collision entities as children
-    // of the appropriate level entity
-    let mut level_to_wall_locations: HashMap<
-        Entity,
-        HashSet<GridCoords>,
-    > = HashMap::new();
-
-    wall_query.for_each(|(&grid_coords, parent)| {
-        // An intgrid tile's direct parent will be a layer
-        // entity, not the level entity To get the
-        // level entity, you need the tile's grandparent.
-        // This is where parent_query comes in.
-        if let Ok(grandparent) =
-            parent_query.get(parent.get())
-        {
-            level_to_wall_locations
-                .entry(grandparent.get())
-                .or_insert(HashSet::new())
-                .insert(grid_coords);
-        }
-    });
-
-    if !wall_query.is_empty() {
-        level_query.for_each(|(level_entity, level_handle)| {
-            if let Some(level_walls) = level_to_wall_locations.get(&level_entity) {
-                let level = levels
-                    .get(level_handle)
-                    .expect("Level should be loaded by this point");
-
-                let LayerInstance {
-                    c_wid: width,
-                    c_hei: height,
-                    grid_size,
-                    ..
-                } = level
-                    .level
-                    .layer_instances
-                    .clone()
-                    .expect("Level asset should have layers")[0];
-
-                // combine wall tiles into flat "plates" in each individual row
-                let mut plate_stack: Vec<Vec<Plate>> = Vec::new();
-
-                for y in 0..height {
-                    let mut row_plates: Vec<Plate> = Vec::new();
-                    let mut plate_start = None;
-
-                    // + 1 to the width so the algorithm "terminates" plates that touch the right
-                    // edge
-                    for x in 0..width + 1 {
-                        match (plate_start, level_walls.contains(&GridCoords { x, y })) {
-                            (Some(s), false) => {
-                                row_plates.push(Plate {
-                                    left: s,
-                                    right: x - 1,
-                                });
-                                plate_start = None;
-                            }
-                            (None, true) => plate_start = Some(x),
-                            _ => (),
-                        }
-                    }
-
-                    plate_stack.push(row_plates);
-                }
-
-                // combine "plates" into rectangles across multiple rows
-                let mut wall_rects: Vec<Rect> = Vec::new();
-                let mut previous_rects: HashMap<Plate, Rect> = HashMap::new();
-
-                // an extra empty row so the algorithm "terminates" the rects that touch the top
-                // edge
-                plate_stack.push(Vec::new());
-
-                for (y, row) in plate_stack.iter().enumerate() {
-                    let mut current_rects: HashMap<Plate, Rect> = HashMap::new();
-                    for plate in row {
-                        if let Some(previous_rect) = previous_rects.remove(plate) {
-                            current_rects.insert(
-                                *plate,
-                                Rect {
-                                    top: previous_rect.top + 1,
-                                    ..previous_rect
-                                },
-                            );
-                        } else {
-                            current_rects.insert(
-                                *plate,
-                                Rect {
-                                    bottom: y as i32,
-                                    top: y as i32,
-                                    left: plate.left,
-                                    right: plate.right,
-                                },
-                            );
-                        }
-                    }
-
-                    // Any plates that weren't removed above have terminated
-                    wall_rects.append(&mut previous_rects.values().copied().collect());
-                    previous_rects = current_rects;
-                }
-
-                commands.entity(level_entity).with_children(|level| {
-                    // Spawn colliders for every rectangle..
-                    // Making the collider a child of the level serves two purposes:
-                    // 1. Adjusts the transforms to be relative to the level for free
-                    // 2. the colliders will be despawned automatically when levels unload
-                    for wall_rect in wall_rects {
-                        level
-                            .spawn((Collider::cuboid(
-                                (wall_rect.right as f32 - wall_rect.left as f32 + 1.)
-                                    * grid_size as f32
-                                    / 2.,
-                                (wall_rect.top as f32 - wall_rect.bottom as f32 + 1.)
-                                    * grid_size as f32
-                                    / 2.,
-                            ),
-                                    RigidBody::Fixed,
-                                    Friction{
-                                        coefficient: 0.1,
-                                        combine_rule:
-                                        CoefficientCombineRule::Min,
-                                    },
-                                    Transform::from_xyz(
-                                        (wall_rect.left + wall_rect.right + 1) as f32 * grid_size as f32
-                                            / 2.,
-                                        (wall_rect.bottom + wall_rect.top + 1) as f32 * grid_size as f32
-                                            / 2.,
-                                        0.,
-                                    ),
-                                    GlobalTransform::default()));
-                    }
-                });
-            }
-        });
-    }
-}
-
-
 
 fn setup_system(
     mut commands: Commands,
@@ -272,13 +81,16 @@ fn setup_system(
             transform: Transform::from_xyz(100., 0., 5.),
             ..default()
         },
-        player::Direction::Up,
         Player {
             health: 10,
             attack_power: 10,
             armor: 10,
+            acceleration: 800.0,
+            deceleration: 700.0,
+            max_speed: 300.0,
         },
         RigidBody::Dynamic,
+        Velocity::zero(),
         LockedAxes::ROTATION_LOCKED,
         GravityScale(0.0),
         Collider::cuboid(64.0 / 2.0, 64.0 / 2.0),
